98509f48486f674084ab4f11d46bc353cc47d36b
Joyce Ubale <joyceubale@Joyces-MacBook-Air.local>
2024-01-29 16:57:36 +0530

    done

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,9 @@
+import react from "react";
+import Book from "./components/Book";
+
+function App() {
+  return (
+    <Book/>
+  );
+}
+export default App;
---
+++ vite-project/src/components/Book.jsx
@@ -0,0 +1,64 @@
+// Book.js
+
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import './Book.css';
+
+function Book() {
+  const [books, setBooks] = useState([]);
+  const [searchBook, setSearchBook] = useState('');
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const result = await axios.get('https://reactnd-books-api.udacity.com/books', {
+          headers: { 'Authorization': 'whatever-you-want' }
+        });
+
+        setBooks(result.data.books);
+      } catch (error) {
+        console.log("Status Code : ", error.response.status);
+        console.log("Website not found");
+      }
+    };
+
+    fetchUserData();
+  }, []);
+
+  const handleSearch = (e) => {
+    setSearchBook(e.target.value);
+  };
+
+  const filteredBooks = books.filter((book) =>
+    book.title.toLowerCase().includes(searchBook.toLowerCase())
+  );
+
+  return (
+    <>
+      <div className="navbar">
+        <h1>Kalvium Books</h1>
+        <input
+          type="text"
+          placeholder="Search for books..."
+          value={searchBook}
+          onChange={handleSearch}
+        />
+        <button>REGISTER</button>
+      </div>
+      <div className="container">
+        {filteredBooks.map((book, index) => (
+          <div key={index} className="item">
+            <h2 className="title">{book.title}</h2>
+            <div className="details">
+              <img src={book.imageLinks.smallThumbnail} alt={book.title} className="book-img" />
+            </div>
+            <p className="book-author">{book.authors[0]}</p>
+            <p className="book-rating">Rating: {book.averageRating}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+export default Book;
